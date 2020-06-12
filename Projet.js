717f23fce53c66549a69342f991437163bd2e544
Léo Le Barazer <leo.lebarazer4@gmail.com>
2020-06-12 18:34:41 +0200

    ajout video index

--- Projet.js
+++ Projet.js
@@ -29,3 +29,21 @@ function addline(){
 function scroll_wp(){
 
 }
+
+function toggleMute() {
+
+  var video=document.getElementById("videoId");
+
+  if(video.muted){
+    video.muted = false;
+  } else {
+    debugger;
+    video.muted = true;
+    video.play()
+  }
+
+}
+
+$(document).ready(function(){
+  setTimeout(toggleMute,3000);
+})
